c2ec5d679fc30735058319cfff3868dc100cbc05
KauhikNilay <30530804+KauhikNilay@users.noreply.github.com>
2018-04-05 23:23:13 +0530

    offline activities completed. TO DO->  link the pdf to detail button

--- app/src/main/java/inc/iris/techudbhav/CampusAmbassador.java
+++ app/src/main/java/inc/iris/techudbhav/CampusAmbassador.java
@@ -12,7 +12,7 @@ public class CampusAmbassador extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_soccer_bot);
+        setContentView(R.layout.activity_campus_ambassador);
         Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         if(getSupportActionBar() != null)
