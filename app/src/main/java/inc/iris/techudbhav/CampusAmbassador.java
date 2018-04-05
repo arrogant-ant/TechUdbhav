e76127b994bb784e6c06d72e1cef064e03a9c42b
KauhikNilay <30530804+KauhikNilay@users.noreply.github.com>
2018-04-05 10:09:57 +0530

    SplashScreen and Gallery completed.Photos need to be replaced in Gallery

--- app/src/main/java/inc/iris/techudbhav/CampusAmbassador.java
+++ app/src/main/java/inc/iris/techudbhav/CampusAmbassador.java
@@ -12,7 +12,7 @@ public class CampusAmbassador extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_campus_ambassador);
+        setContentView(R.layout.activity_bytethe_bits);
         Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         if(getSupportActionBar() != null)
