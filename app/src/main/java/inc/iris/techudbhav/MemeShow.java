1af0d0d0994e060c394721fcef2205aba9085dbd
KauhikNilay <30530804+KauhikNilay@users.noreply.github.com>
2018-04-06 01:15:34 +0530

    Two online events added: Graffiti and Meme Show

---
+++ app/src/main/java/inc/iris/techudbhav/MemeShow.java
@@ -0,0 +1,17 @@
+package inc.iris.techudbhav;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MemeShow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meme_show);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
